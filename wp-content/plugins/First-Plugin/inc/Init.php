2a61a75f22a7ccd8521a403729971f908e35d7be
Cihat Pala <cihat_pala@hotmail.com>
2020-09-04 11:44:49 +0300

    SubPage Complate

--- wp-content/plugins/First-Plugin/inc/Init.php
+++ wp-content/plugins/First-Plugin/inc/Init.php
@@ -1,5 +1,7 @@
 <?php
-
+/**
+ * @package
+ */
 namespace Inc;
 
 final class Init{
--- wp-content/plugins/First-Plugin/inc/Pages/Admin.php
+++ wp-content/plugins/First-Plugin/inc/Pages/Admin.php
@@ -7,41 +7,58 @@ namespace Inc\Pages;
 
 use \Inc\Base\BaseController;
 use \Inc\Api\SettingsApi;
+
 class Admin extends BaseController{
 
     public $settings;
     public $pages = array();
+    public $subpages = array();
 
     public function __construct(){
         $this->settings = new SettingsApi();
 
-        $this->pages = array ( 
+        $this->pages = array( 
         
             array(
                 'page_title' => 'Ibb Haber Pluginssss', 
-                'menu_title' => 'Ibb Haber Plugin... by Cihat Pala', 
+                'menu_title' => 'Ibb Haber Plugin', 
                 'capability' => 'manage_options', 
-                'menu_slug' => 'ibbhaber-plugin', 
-                'callback' => function () {echo '<h1> Cihat Pala Plugin </h1>';}, 
+                'menu_slug' => 'ibbhaber_plugin', 
+                'callback' => function () {echo '<h1> Cihat Pala Plugin</h1>';}, 
                 'icon_url' => 'dashicons-store', 
                 'position' => 110
-            ),
-
-            array(
-                'page_title' => 'Bu da bir Eklenti', 
-                'menu_title' => 'BudaEklenti', 
-                'capability' => 'manage_options', 
-                'menu_slug' => 'test-plugin', 
-                'callback' => function () {echo '<h1> External </h1>';}, 
-                'icon_url' => 'dashicons-external', 
-                'position' => 9
             )
-
             );
+
+            $this->subpages = array( 
+                array(
+                    'parent_slug' => 'ibbhaber_plugin', 
+                    'page_title' => 'Custom Post Type', 
+                    'menu_title' => 'CPT', 
+                    'capability' => 'manage_options', 
+                    'menu_slug' => 'ibbhaber_cpt', 
+                    'callback' => function () {echo '<h1>Custom Post Types (CPT) Manager</h1>';}
+                ),
+                array(
+                    'parent_slug' => 'ibbhaber_plugin', 
+                    'page_title' => 'Custom Taxonomies', 
+                    'menu_title' => 'Taxonomies', 
+                    'capability' => 'manage_options', 
+                    'menu_slug' => 'alecaddd_taxonomies', 
+                    'callback' => function() { echo '<h1>Taxonomies Manager</h1>'; }
+                ),
+                array(
+                    'parent_slug' => 'ibbhaber_plugin', 
+                    'page_title' => 'Custom Widgets', 
+                    'menu_title' => 'Widgets', 
+                    'capability' => 'manage_options', 
+                    'menu_slug' => 'ibbhaber_widgets', 
+                    'callback' => function () {echo '<h1>Widget Manager</h1>';}
+                )
+                );
     }
 
-    public function register(){
-        // add_action('admin_menu', array($this,'add_admin_pages'));
-        $this->settings->addPages( $this->pages )->register();
-    }
+	public function register(){
+		$this->settings->addPages( $this->pages )->withSubPage( 'Dashboard' )->addSubPages( $this->subpages )->register();
+	}
 }
