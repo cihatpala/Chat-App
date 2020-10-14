16a608fe2b9a4167a43c6c9192ebac501fd25780
Cihat Pala <cihat_pala@hotmail.com>
2020-10-14 13:04:02 +0300

    Code Cleanup (BaseController.php -> managers to Admin.php)

--- wp-content/plugins/First-Plugin/inc/Base/BaseController.php
+++ wp-content/plugins/First-Plugin/inc/Base/BaseController.php
@@ -10,9 +10,23 @@
      public $plugin_url;
      public $plugin;
 
+     public $managers = array();
+
      public function __construct(){
         $this->plugin_path = plugin_dir_path( dirname(__FILE__, 2) );
         $this->plugin_url = plugin_dir_url( dirname(__FILE__, 2) );
         $this->plugin = plugin_basename( dirname(__FILE__, 3) ) . '/first-plugin.php';
+
+        $this->managers = array(
+            'cpt_manager' => 'Activate CPT Manager',
+            'taxonomy_manager' => 'Activate Taxonomy Manager',
+            'media_widget' => 'Activate Media Widget',
+            'gallery_manager' => 'Activate Gallary Manager',
+            'testimonial_manager' => 'Activate Testimonial',
+            'templates_manager' => 'Activate Templates Manager',
+            'login_manager' => 'Activate Login Manager',
+            'membership_manager' => 'Activate Membership Manager',
+            'chat_manager' => 'Activate Chat Manager'
+        );
      }
  }
