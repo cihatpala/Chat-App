f8deeb88069ee2d7f5e547b03a41fec6996ad992
Cihat PALA <cihat_pala@hotmail.com>
2020-11-25 00:43:27 +0300

    collect, validate, reset form message

--- wp-content/plugins/First-Plugin/src/js/form.js
+++ wp-content/plugins/First-Plugin/src/js/form.js
@@ -2,11 +2,42 @@ document.addEventListener('DOMContentLoaded', function(e){
     let testimonialForm = document.getElementById('ibbhaber-testimonial-form');
     testimonialForm.addEventListener('submit',(e) => {
         e.preventDefault();
-        console.log('Prevent form submit');
 
         //reset the form messages
-        //validate the e-mail
+        resetMessages();
         //collect all the data
+        let data = {
+            name: testimonialForm.querySelector('[name = "name"]').value,
+            email: testimonialForm.querySelector('[name = "email"]').value,
+            message: testimonialForm.querySelector('[name = "message"]').value
+        }
+        console.log(data);
+
+        //validate everything
+        if(!data.name){
+            testimonialForm.querySelector('[data-error="invalidName"]').classList.add('show');
+            return;
+        }
+        if(!validateEmail(data.email)){
+            testimonialForm.querySelector('[data-error="invalidEmail"]').classList.add('show');
+            return;
+        }
+        if(!data.message){
+            testimonialForm.querySelector('[data-error="invalidMessage"]').classList.add('show');
+            return;
+        }
+
         //ajax http post request
-    })
+        let url = testimonialForm.dataset.url;
+        console.log(url);
+    });
 });
+
+function resetMessages(){
+    document.querySelectorAll('.field-msg').forEach(f=> f.classList.remove('show'));
+}
+
+function validateEmail(email){
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
--- wp-content/plugins/First-Plugin/templates/contact-form.php
+++ wp-content/plugins/First-Plugin/templates/contact-form.php
@@ -1,18 +1,18 @@
 <form id="ibbhaber-testimonial-form"action="#" method="post" data-url="<?php echo admin_url('admin-ajax.php'); ?>">
 
 	<div class="field-container">
-		<input type="text" class="field-input" placeholder="Your Name" id="name" name="name" required>
-		<small class="field-msg error">Your Name is Required</small>
+		<input type="text" class="field-input" placeholder="Adınız" id="name" name="name">
+		<small class="field-msg error" data-error="invalidName">Ad kutusu boş bırakılamaz!</small>
 	</div>
 
 	<div class="field-container">
-		<input type="email" class="field-input" placeholder="Your Email" id="email" name="email" required>
-		<small class="field-msg error">Your Email is Required</small>
+		<input type="text" class="field-input" placeholder="Email Adresiniz" id="email" name="email">
+		<small class="field-msg error"  data-error="invalidEmail">Lütfen Email adresi giriniz. </small>
 	</div>
 
 	<div class="field-container">
-		<textarea name="message" id="message" class="field-input" placeholder="Your Message" required></textarea>
-		<small class="field-msg error">A Message is Required</small>
+		<textarea name="message" id="message" class="field-input" placeholder="Mesajınız"></textarea>
+		<small class="field-msg error" data-error="invalidMessage">Mesaj kutusu boş bırakılamaz!</small>
 	</div>
 	
 	<div class="field-container">
